59515fde3d0c70ea33af20b14ce399459303264f
Kee GUON <kee@guon.kr>
2022-04-26 17:42:15 +0700

    Use SSH keys

--- code.js
+++ code.js
@@ -3,5 +3,29 @@ console.log('code')
 // git status
 // git add .
 // git commit -m "Initial commit"
-// git remote origin https://github.com/malgl/git-ex.git
+// git remote add origin https://github.com/malgl/git-ex.git
+// git push origin master
+// git log --all --graph
+
+// git clone https://github.com/malgl/git-ex.git
+// git clone https://github.com/malgl/git-ex.git folder-name
+
+// SSH keys
+// ls ~/.ssh
+// ssh-keygen -t ed25519 -C "v0v@naver.com"
+// kig1ssh
+// Services
+// OpenSSH Authentication Agent -> Automatic -> Apply -> Start -> Running -> OK
+// ssh-add $HOME/.ssh/id_ed25519
+// kig1ssh
+// runemacs $HOME/.ssh/id_ed25519.pub
+// github.com -> Settings -> SSH and GPG keys -> New SSH key -> Add SSH key
+// git remote -v
+// git remote remove origin
+// git remote -v
+// git remote add origin https://github.com/malgl/git-ex.git
+// git remote -v
+// git status
+// git add .
+// git commit -m "Use SSH keys"
 // git push origin master
